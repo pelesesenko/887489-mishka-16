41642a5d6285b2947ad152534055cda84e64581e
pelesesenko <kw04@i.ua>
2019-04-28 00:51:42 +0300

    Спрайт и автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,18 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var cheerio = require("gulp-cheerio");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
 var server = require("browser-sync").create();
 
-gulp.task("css", function () {
+gulp.task("css", function() {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
@@ -16,14 +25,76 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.mini.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran(), //{ progressive: true }
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 80 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("rmAttr", function() {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(cheerio({
+      run: function($) {
+        $("[fill]").removeAttr("fill");
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore()) //{inlineSvg: true}
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+// gulp.task("html", function() {
+//   return gulp.src("source/*.html")
+//     .pipe(posthtml([
+//       include()
+//     ]))
+//     .pipe(gulp.dest("build"));
+// });
+
+gulp.task("copy", function() {
+  return gulp.src(
+      ["source/fonts/**?*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/*.ico",
+        "source/*.html"
+      ], { base: "source" }
+    )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("server", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +102,20 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("source/*.html", gulp.series("refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+})
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite"
+));
+
+gulp.task("start", gulp.series("build", "server"));
--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,4 @@
+'use strict'
 let menuToggleBtn = document.querySelector('[data-mobile-menu="toggle"]');
 let menuItems = document.querySelectorAll('[data-mobile-menu="item"]');
 let isMenuOpen = true;
@@ -5,59 +6,59 @@ let isMobile = false;
 
 
 function openMenu() {
-    menuToggleBtn.classList.add('page-header__btn-menu--close');
-    for (let i = 0; i < menuItems.length; i++) {
-        menuItems[i].style.display = '';
-        isMenuOpen = true;
-    }
+  menuToggleBtn.classList.add('page-header__btn-menu--close');
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].style.display = '';
+    isMenuOpen = true;
+  }
 }
 
 function closeMenu() {
-    menuToggleBtn.classList.remove('page-header__btn-menu--close');
-    for (let i = 0; i < menuItems.length; i++) {
-        menuItems[i].style.display = 'none';
-        isMenuOpen = false;
-    }
+  menuToggleBtn.classList.remove('page-header__btn-menu--close');
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].style.display = 'none';
+    isMenuOpen = false;
+  }
 }
 
 function toggleMenu() {
-    if (isMenuOpen) {
-        closeMenu();
-        return;
-    }
-    openMenu();
+  if (isMenuOpen) {
+    closeMenu();
+    return;
+  }
+  openMenu();
 }
 
 if (document.documentElement.clientWidth < 768) {
-    isMobile = true;
-    closeMenu();
-    menuToggleBtn.addEventListener('click', toggleMenu);
-    menuToggleBtn.style.display = 'block';
+  isMobile = true;
+  closeMenu();
+  menuToggleBtn.addEventListener('click', toggleMenu);
+  menuToggleBtn.style.display = 'block';
 }
 
-let resizeHandler = (ms) => {
-    let isThrottled = false;
-    let wrapper = () => {
-        if (isThrottled) return;
-        isThrottled = true;
-        if (document.documentElement.clientWidth < 768 && isMobile === false) {
-            isMobile = true;
-            closeMenu();
-            menuToggleBtn.style.display = 'block';
-            menuToggleBtn.addEventListener('click', toggleMenu);
-        }
-        if (document.documentElement.clientWidth >= 768 && isMobile === true) {
-            isMobile = false;
-            openMenu();
-            menuToggleBtn.style.display = '';
-            menuToggleBtn.removeEventListener('click', toggleMenu);
-        }
-        setTimeout(() => {
-            isThrottled = false;
-            wrapper();
-        }, ms)
+let resizeHandler = function(ms) {
+  let isThrottled = false;
+  let wrapper = function() {
+    if (isThrottled) return;
+    isThrottled = true;
+    if (document.documentElement.clientWidth < 768 && isMobile === false) {
+      isMobile = true;
+      closeMenu();
+      menuToggleBtn.style.display = 'block';
+      menuToggleBtn.addEventListener('click', toggleMenu);
     }
-    return wrapper;
+    if (document.documentElement.clientWidth >= 768 && isMobile === true) {
+      isMobile = false;
+      openMenu();
+      menuToggleBtn.style.display = '';
+      menuToggleBtn.removeEventListener('click', toggleMenu);
+    }
+    setTimeout(function() {
+      isThrottled = false;
+      wrapper();
+    }, ms)
+  }
+  return wrapper;
 }
 
 window.addEventListener("resize", resizeHandler(50));
@@ -67,28 +68,30 @@ window.addEventListener("resize", resizeHandler(50));
 let modalWindows = document.querySelectorAll('[data-modal]');
 
 for (let i = 0; i < modalWindows.length; i++) {
-    let nameModal = '"' + modalWindows[i].dataset.modal + '"';
-    modalWindows[i].openers = document.querySelectorAll(`[data-modal-opener=${nameModal}]`);
-    for (let j = 0; j < modalWindows[i].openers.length; j++) {
-        modalWindows[i].openers[j].addEventListener("click", (event) => {
-            event.preventDefault();
-            modalWindows[i].classList.add("modal--show");
-        })
-    }
-    modalWindows[i].closingBtn = modalWindows[i].querySelector('[data-modal-btn]');
-    modalWindows[i].closingBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        modalWindows[i].classList.remove("modal--show");
+  let modalWindow = modalWindows[i];
+  let selector = '[data-modal-opener=' + '"' + modalWindow.dataset.modal + '"' + ']';
+  modalWindow.openers = document.querySelectorAll(selector);
+
+  for (let j = 0; j < modalWindow.openers.length; j++) {
+    modalWindow.openers[j].addEventListener("click", function(event) {
+      event.preventDefault();
+      modalWindow.classList.add("modal--show");
     })
+  }
+  modalWindow.closingBtn = modalWindow.querySelector('[data-modal-btn]');
+  modalWindow.closingBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalWindow.classList.remove("modal--show");
+  })
 }
 
 window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode == 27) {
-        for (let i = 0; i < modalWindows.length; i++) {
-            if (modalWindows[i].classList.contains("modal--show")) {
-                evt.preventDefault();
-                modalWindows[i].classList.remove("modal--show");
-            }
-        }
+  if (evt.keyCode == 27) {
+    for (let i = 0; i < modalWindows.length; i++) {
+      if (modalWindows[i].classList.contains("modal--show")) {
+        evt.preventDefault();
+        modalWindows[i].classList.remove("modal--show");
+      }
     }
+  }
 });
